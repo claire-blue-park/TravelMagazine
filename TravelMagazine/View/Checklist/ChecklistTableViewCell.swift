5b71542530699b89764cc5fc0e5abb07010dc7ce
chhue <chhue96@gmail.com>
2025-01-03 20:30:01 +0900

    Chore: TableViewController 및 Cell의 스위프트 파일 생성
    각 탭에 해당하는 TableViewController 및 Cell의 스위프트 파일 생성 및 스토리보드 연결

---
+++ TravelMagazine/View/Checklist/ChecklistTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChecklistTableViewCell.swift
+//  TravelMagazine
+//
+//  Created by Claire on 1/3/25.
+//
+
+import UIKit
+
+class ChecklistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
