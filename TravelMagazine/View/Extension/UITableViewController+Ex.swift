fe08b8413fef9268874a0fa04e3ea95021d301fe
chhue <chhue96@gmail.com>
2025-01-04 21:23:24 +0900

    Feat: TableView Cell UI 스토리보드 구성

---
+++ TravelMagazine/View/Extension/UITableViewController+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewController+Ex.swift
+//  TravelMagazine
+//
+//  Created by Claire on 1/3/25.
+//
+
+import UIKit
+
+extension UITableViewController {
+    open override func viewDidLoad() {
+        self.tableView.backgroundColor = .background
+    }
+}
